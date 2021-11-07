5cab0a98692554917a4bf0a55d9e48f8d5fd36f7
Yusuke Kadowaki <yusuke.kadowaki.1231@gmail.com>
2021-11-07 19:09:23 +0900

    implemented mock test and transfer api (no.14,15)

--- util/random.go
+++ util/random.go
@@ -37,7 +37,7 @@ func RandomMoney() int64 {
 }
 
 func RnadomCurrency() string {
-	currencies := []string{"EUR", "USD", "CAD"}
+	currencies := []string{EUR, USD, CAD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
