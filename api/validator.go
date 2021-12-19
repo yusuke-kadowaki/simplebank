e7058414759145551a0e3389cb86366ecdaaa4b4
Yusuke Kadowaki <yusuke.kadowaki.1231@gmail.com>
2021-12-20 02:13:05 +0900

    add comment

--- api/validator.go
+++ api/validator.go
@@ -6,6 +6,7 @@ import (
 )
 
 var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
+	// this syntax means taking the interface{} as string
 	if currency, ok := fieldLevel.Field().Interface().(string); ok {
 		// check if the currency is supported
 		return util.IsSupportedCurrency(currency)
